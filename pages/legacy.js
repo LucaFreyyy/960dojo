d169c115915aeb9fa1a2b028c77ac1f0b8a2ac17
Luca Frey <uohsq@student.kit.edu>
2025-07-17 16:03:24 +0200

    removed dom issues

--- pages/legacy.js
+++ pages/legacy.js
@@ -247,12 +247,27 @@ export default function LegacyPage() {
                 </div>
 
                 <div id="fullScreenLoadingOverlay">
-                    <div class="loader">
+                    <div className="loader">
                         <svg width="60" height="60" viewBox="0 0 50 50">
-                            <circle cx="25" cy="25" r="20" fill="none" stroke="#fff" stroke-width="5" stroke-linecap="round"
-                                stroke-dasharray="31.4 31.4" transform="rotate(-90 25 25)">
-                                <animateTransform attributeName="transform" type="rotate" from="0 25 25" to="360 25 25" dur="1s"
-                                    repeatCount="indefinite" />
+                            <circle
+                                cx="25"
+                                cy="25"
+                                r="20"
+                                fill="none"
+                                stroke="#fff"
+                                strokeWidth="5"
+                                strokeLinecap="round"
+                                strokeDasharray="31.4 31.4"
+                                transform="rotate(-90 25 25)"
+                            >
+                                <animateTransform
+                                    attributeName="transform"
+                                    type="rotate"
+                                    from="0 25 25"
+                                    to="360 25 25"
+                                    dur="1s"
+                                    repeatCount="indefinite"
+                                />
                             </circle>
                         </svg>
                     </div>
