1a925d9486ca1fc7d67ef820a5a2ebe3ec6e92ef
LucaFreyyy <uohsq@student.kit.edu>
2026-04-03 23:01:42 +0200

    fixed phone layout

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
     return (
         <Html>
             <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
                 <link rel="icon" href="/favicon-32x32.png" />
                 <link
                     href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Pacifico&display=swap"
