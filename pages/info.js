789f0e12a54a273dd15364937314fe9a5d49206d
LucaFreyyy <uohsq@student.kit.edu>
2026-04-12 03:15:41 +0200

    added chessonfire

--- pages/info.js
+++ pages/info.js
@@ -24,6 +24,7 @@ export default function InfoPage() {
                         Grenke tournament begins.
                     </p>
                     <p>I hope you enjoy my website and that it helps you improve your Chess960 skills!</p>
+                    <p>For video resources to study Chess960, I recommend <a href="https://www.youtube.com/playlist?list=PLU8ZSPKBpOES_D-1RumtqGco1n5LIj3Kq" target="_blank" rel="noopener noreferrer">this</a> playlist by ChessOnFire</p>
                     <p>If you want to reach out to me, you can do so via email at <a href="mailto:lucafreyq@gmail.com">lucafreyq@gmail.com</a></p>
                 </section>
             </main>
